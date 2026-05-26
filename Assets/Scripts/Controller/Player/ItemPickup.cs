c2376b2baeb1b02b8afc9f3b0403a84d8c7e4316
Xyberov <albel1603@yandex.ru>
2026-05-26 11:31:34 +0500

    Добавлен звук подбора монет

--- Assets/Scripts/Controller/Player/ItemPickup.cs
+++ Assets/Scripts/Controller/Player/ItemPickup.cs
@@ -6,6 +6,8 @@ public class ItemPickup : MonoBehaviour
     [SerializeField] private DropType dropType;
     [SerializeField] private int amount = 1;
 
+    [SerializeField] private AudioClip pickupSound;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
@@ -14,7 +16,11 @@ public class ItemPickup : MonoBehaviour
             other.GetComponent<PlayerCombat>()?.AddArrows(amount);
 
         if (dropType == DropType.Coin)
+        {
             other.GetComponent<PlayerCoins>()?.AddCoins(amount);
+            if (pickupSound != null)
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+        }
 
         Destroy(gameObject);
     }
