820cddeefc22d2db5daafd527048643ed99a6b99
Xyberov <albel1603@yandex.ru>
2026-05-26 11:16:20 +0500

    Добавлена память монет между сессиями

--- Assets/Scripts/Controller/Player/PlayerCoins.cs
+++ Assets/Scripts/Controller/Player/PlayerCoins.cs
@@ -7,6 +7,8 @@ public class PlayerCoins : MonoBehaviour
     void Awake()
     {
         Model = new CoinModel();
+        Model.Add(PlayerPrefs.GetInt("Coins", 0));
+        Model.OnCountChanged += count => PlayerPrefs.SetInt("Coins", count);
     }
 
     public void AddCoins(int amount)
